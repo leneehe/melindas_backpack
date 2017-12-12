c27c1f2512e9ee01833867ce6150426b1115f61e
Lena <fatmeow@gmail.com>
2017-12-12 15:56:07 -0500

    Stretch: use extract temp to query to return same variable value

--- backpack.rb
+++ backpack.rb
@@ -10,21 +10,26 @@ class Backpack
   end
 
   def prepare
-    @weather_condition = @attributes[:weather]
-    @day_of_week = @attributes[:day_of_week]
-
     prepare_for_weather
     prepare_for_gym_day
     prepare_the_food
   end
 
+  def weather_condition
+    @attributes[:weather]
+  end
+
+  def day_of_week
+    @attributes[:day_of_week]
+  end
+
   def prepare_for_weather
     # Ensure appropriate clothing is added to backpack
-    if @weather_condition == 'rainy'
+    if weather_condition == 'rainy'
       @items << 'pants'
       @items << 'shirt'
       @items << 'umbrella'
-    elsif @weather_condition == 'cold'
+    elsif weather_condition == 'cold'
       @items << 'pants'
       @items << 'shirt'
       @items << 'jacket'
@@ -36,14 +41,14 @@ class Backpack
 
   def prepare_for_gym_day
     # Ensure gym shoes are added to backpack if it's a gym day-Mondays and Thursdays.
-    if @day_of_week == 'monday' || @day_of_week == 'thursday'
+    if day_of_week == 'monday' || day_of_week == 'thursday'
       @items << 'gym shoes'
     end
   end
 
   def prepare_the_food
     # Bring a packed lunch on all weekdays
-    if @day_of_week != 'saturday' && @day_of_week != 'sunday'
+    if day_of_week != 'saturday' && day_of_week != 'sunday'
       @items << 'packed lunch'
     end
   end
